a88f248e35c10a8032e668f121837fc10a9b6e85
Justin Varga <77.j.varga@gmail.com>
2024-04-18 20:47:24 -0400

    Improved camera
    Separated camera movement and collision. Clipping much less noticeable, but still present.

---
+++ Assets/Scripts/CameraCollision.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class CameraCollision : MonoBehaviour
+{
+    public float minDistance = 1f; // Minimum distance between camera and guide
+    public float maxDistance = 4f; // Maximum distance between camera and guide
+    private float distance; // Distance between camera and player
+    private Vector3 dollyDir;
+    public float smoothTime = 0.5f;
+    public GameObject cameraGuide;
+
+    void Awake()
+    {
+        dollyDir = transform.localPosition.normalized;
+        distance = Vector3.Distance(transform.position, cameraGuide.transform.position);
+    }
+
+    void FixedUpdate()
+    {
+        // Try to move camera
+        Vector3 targetCamPos = transform.parent.TransformPoint(dollyDir * maxDistance);
+
+        // Check for clipping
+        RaycastHit hit;
+        if (Physics.Linecast(cameraGuide.transform.position, targetCamPos, out hit))
+        {
+            Debug.DrawLine(cameraGuide.transform.position, hit.point, Color.red);
+            distance = Mathf.Clamp(hit.distance * 0.75f, minDistance, maxDistance);
+        }
+        else
+        {
+            distance = maxDistance;
+        }
+
+        transform.localPosition = Vector3.Lerp(transform.localPosition, dollyDir * distance, Time.deltaTime * smoothTime);
+    }
+}
--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,61 +5,57 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public GameObject player; // Reference to the player sphere GameObject
-    public GameObject guide; // Reference to the satellite sphere GameObject
 
-    private Rigidbody playerRB;
-
-    private float maxDistance; // Maximum distance between camera and player sphere
-    private float distance; // Distance between camera and player
-    public float sensitivityX = 5f; // Mouse sensitivity for horizontal movement
-    public float sensitivityY = 5f; // Mouse sensitivity for vertical movement
+    public float cameraSpeed = 120f;
+    
+    public float sensitivityX = 300f; // Mouse sensitivity for horizontal movement
+    public float sensitivityY = 300f; // Mouse sensitivity for vertical movement
     public float minYAngle = -60f; // Minimum vertical angle for the camera
     public float maxYAngle = 60f; // Maximum vertical angle for the camera
 
-    private float currentX = 0f; // Current horizontal rotation of the camera
-    private float currentY = 0f; // Current vertical rotation of the camera
+    private float mouseX, mouseY;
+    private float finalInputX, finalInputZ;
+    private float rotX = 0f; // Current horizontal rotation of the camera
+    private float rotY = 0f; // Current vertical rotation of the camera
 
-    private Vector3 guideOffset, targetGuidePosition;
+    private Vector3 targetGuidePosition;
+    public float guideOffset;
 
-    public float smoothTime = 0.3f;
+    public float smoothTime = 0.5f;
 
-    private void Start()
+    void Awake()
     {
-        // Get calculations from scene
-        maxDistance = Vector3.Distance(transform.position, player.transform.position);
-        guideOffset = guide.transform.position - player.transform.position;
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rotX = rot.x;
+        rotY = rot.y;
+    }
 
-        playerRB = player.GetComponent<Rigidbody>();
+    void Update()
+    {
+        float inputX = Input.GetAxis("RightStickHorizontal");
+        float inputZ = Input.GetAxis("RightStickVertical");
+
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
+
+        finalInputX = inputX + mouseX;
+        finalInputZ = inputZ + mouseY;
+
+        rotY += finalInputX * sensitivityX * Time.deltaTime;
+        rotX += finalInputZ * sensitivityY * Time.deltaTime;
+        rotX = Mathf.Clamp(rotX, minYAngle, maxYAngle);
+
+        Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0);
+        transform.rotation = localRotation;
     }
 
     private void LateUpdate()
     {
-        // Get mouse input for camera rotation
-        currentX += Input.GetAxis("Mouse X") * sensitivityX;
-        currentY -= Input.GetAxis("Mouse Y") * sensitivityY;
-        
-        // Clamp vertical rotation angle within limits
-        currentY = Mathf.Clamp(currentY, minYAngle, maxYAngle);
-
-        // Calculate camera rotation based on mouse input
-        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
-
-        // Check for clipping
-        RaycastHit hit;
-        distance = Physics.Raycast(guide.transform.position, transform.position, out hit, maxDistance, ~LayerMask.NameToLayer("UI")) ? hit.distance : maxDistance;
-
-        // Set camera position relative to player sphere
-        transform.position = player.transform.position - rotation * Vector3.forward * distance;
-
         // Calculate guide position relative to player sphere
-        targetGuidePosition = player.transform.position + rotation * guideOffset;
+        targetGuidePosition = player.transform.position + Vector3.Cross(transform.forward, transform.right).normalized * guideOffset;
 
-        // Smoothly move the guide towards its target position
-        Vector3 velocity = playerRB.velocity;
-        guide.transform.position = Vector3.SmoothDamp(guide.transform.position, targetGuidePosition, ref velocity, smoothTime);
-
-        // Look at the guide sphere to maintain right triangle relationship
-        transform.LookAt(guide.transform);
+        // Move base towards guide
+        transform.position = Vector3.MoveTowards(transform.position, targetGuidePosition, smoothTime);
     }
 
 }
